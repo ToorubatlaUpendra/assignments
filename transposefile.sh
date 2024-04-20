ff76bfb3fc5378ed5de7ab57a9855518ff9cd2cc
ToorubatlaUpendra <upendratoorubatla@gmail.com>
2024-04-20 15:07:47 +0530

    change

--- transposefile.sh
+++ transposefile.sh
@@ -5,7 +5,7 @@ file=$(cat /tmp/file.txt)
 while IFS= read line
 do 
     usage+=$(echo $line | awk '{print$1}')
-    usage+=$(echo $line | awk '{print$2}')
+    # usage+=$(echo $line | awk '{print$2}')
 
 done <<< "$file"
 
